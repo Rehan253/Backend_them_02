3f0722989c430c622fecddfdef8560e05f169e79
Rehan253 <rehan.shafique.253@gmail.com>
2025-10-03 11:41:32 +0200

    feat(user schema): added registration changeset with secure password and role

---
+++ lib/as_backend_theme2/accounts/role.ex
@@ -0,0 +1,17 @@
+defmodule AsBackendTheme2.Accounts.Role do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "roles" do
+    field :name, :string
+    has_many :users, AsBackendTheme2.Accounts.User
+    timestamps()
+  end
+
+  @doc false
+  def changeset(role, attrs) do
+    role
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
---
+++ priv/repo/migrations/20251003092200_update_users_name_and_address.exs
@@ -0,0 +1,12 @@
+defmodule AsBackendTheme2.Repo.Migrations.UpdateUsersNameAndAddress do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      add :address, :string
+      remove :username
+    end
+  end
+end
