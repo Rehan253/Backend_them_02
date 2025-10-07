c1a5da81936dacf3800c4ae852c5e053805b5f9b
Rehan253 <rehan.shafique.253@gmail.com>
2025-10-07 11:30:47 +0200

    fixed issue of user creation

--- lib/as_backend_theme2_web/controllers/user_controller.ex
+++ lib/as_backend_theme2_web/controllers/user_controller.ex
@@ -30,20 +30,23 @@ defmodule AsBackendTheme2Web.UserController do
     render(conn, :index, users: users)
   end
 
-  def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
-      {:ok, user} ->
-        conn
-        |> put_status(:created)
-        |> render(:show, user: user)
+def create(conn, %{"user" => user_params}) do
+  case Accounts.create_user(user_params) do
+    {:ok, user} ->
+      user = AsBackendTheme2.Repo.preload(user, :role)
 
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> put_view(json: AsBackendTheme2Web.UserJSON)
-        |> render(:error, changeset: changeset)
-    end
+      conn
+      |> put_status(:created)
+      |> render(:show, user: user)
+
+    {:error, changeset} ->
+      conn
+      |> put_status(:unprocessable_entity)
+      |> put_view(json: AsBackendTheme2Web.UserJSON)
+      |> render(:error, changeset: changeset)
   end
+end
+
 
   @spec show(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def show(conn, %{"id" => id}) do
