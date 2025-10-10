7ef22728e865057901719018b10b47202e28025c
Rehan253 <rehan.shafique.253@gmail.com>
2025-10-11 00:46:14 +0200

    fixed issue in migration file

--- priv/repo/migrations/20251010133813_create_leave_requests.exs
+++ priv/repo/migrations/20251010133813_create_leave_requests.exs
@@ -1,17 +1,21 @@
-def change do
-  create table(:leave_requests) do
-    add :leave_type, :string, null: false
-    add :start_date, :date, null: false
-    add :end_date, :date, null: false
-    add :reason, :text, null: false
-    add :status, :string, null: false, default: "Pending"
-    add :user_id, references(:users, on_delete: :delete_all), null: false
+defmodule AsBackendTheme2.Repo.Migrations.CreateLeaveRequests do
+  use Ecto.Migration
 
-    timestamps(type: :utc_datetime)
+  def change do
+    create table(:leave_requests) do
+      add :leave_type, :string, null: false
+      add :start_date, :date, null: false
+      add :end_date, :date, null: false
+      add :reason, :text, null: false
+      add :status, :string, null: false, default: "Pending"
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:leave_requests, [:user_id])
+    create constraint(:leave_requests, :end_date_after_start_date,
+      check: "end_date >= start_date"
+    )
   end
-
-  create index(:leave_requests, [:user_id])
-  create constraint(:leave_requests, :end_date_after_start_date,
-    check: "end_date >= start_date"
-  )
 end
