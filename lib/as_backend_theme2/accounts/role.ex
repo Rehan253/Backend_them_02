fbd81112f86a323da10904cb8c3fc06a1d03926a
Shanzay Asad <shanzayyasad@gmail.com>
2025-10-10 12:06:38 +0200

    feat: Update user and role management
    - Enhance user schema with team relationships
    - Improve role management with additional permissions
    - Add better user validation and constraints
    - Update user-role associations for team functionality

--- lib/as_backend_theme2/accounts/role.ex
+++ lib/as_backend_theme2/accounts/role.ex
@@ -2,6 +2,8 @@ defmodule AsBackendTheme2.Accounts.Role do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Jason.Encoder, only: [:id, :name]}
+
   schema "roles" do
     field :name, :string
     has_many :users, AsBackendTheme2.Accounts.User
